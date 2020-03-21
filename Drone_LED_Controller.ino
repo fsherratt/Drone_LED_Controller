e6a1d143ab4657a73007e539b15948e311b10d44
fsherratt <fsherratt@gmail.com>
2020-03-21 16:54:07 +0000

    Added LEG led initialiser

--- Drone_LED_Controller.ino
+++ Drone_LED_Controller.ino
@@ -3,8 +3,13 @@
 
 #define PIN_PORT_RING 12
 #define PIN_STAR_RING A2
+#define LEG_STRIP_1 7
+#define LEG_STRIP_2 4
+#define LEG_STRIP_3 3
+#define LEG_STRIP_4 2
 
 #define RING_PIXELS 16
+#define LEG_PIXELS 16
 
 #define BRIGHTNESS 10 // Max 255
 #define LOOP_DELAY 10 // 100Hz main loop
@@ -14,6 +19,11 @@
 
 NeoPixel_Strobe port_ring_strip = NeoPixel_Strobe(RING_PIXELS, PIN_PORT_RING, NEO_GRBW + NEO_KHZ800);
 NeoPixel_Strobe star_ring_strip = NeoPixel_Strobe(RING_PIXELS, PIN_STAR_RING, NEO_GRBW + NEO_KHZ800);
+
+NeoPixel_Strobe leg_1_strip = NeoPixel_Strobe(LEG_PIXELS, LEG_STRIP_1, NEO_GRB + NEO_KHZ800);
+NeoPixel_Strobe leg_2_strip = NeoPixel_Strobe(LEG_PIXELS, LEG_STRIP_2, NEO_GRB + NEO_KHZ800);
+NeoPixel_Strobe leg_3_strip = NeoPixel_Strobe(LEG_PIXELS, LEG_STRIP_3, NEO_GRB + NEO_KHZ800);
+NeoPixel_Strobe leg_4_strip = NeoPixel_Strobe(LEG_PIXELS, LEG_STRIP_4, NEO_GRB + NEO_KHZ800);
 
 uint16_t gLoopCount;
 uint8_t mode;
@@ -25,6 +35,11 @@ void setup()
 
   initialiseNeoPixel( &port_ring_strip );
   initialiseNeoPixel( &star_ring_strip );
+
+  initialiseNeoPixel( &leg_1_strip );
+  initialiseNeoPixel( &leg_2_strip );
+  initialiseNeoPixel( &leg_3_strip );
+  initialiseNeoPixel( &leg_4_strip );
 }
 
 void loop()
